740ea7f3d48e807fe1ea70b42c2b1063a95cfb46
singhsiddh <singhsiddh@yahoo.com>
2022-01-28 20:59:47 +0000

    dynamic image loading from hhtp url
    Signed-off-by: singhsiddh <singhsiddh@yahoo.com>

--- ParvatiArchana/app/src/main/java/com/example/parvatiarchana/LinerNestedActivity.java
+++ ParvatiArchana/app/src/main/java/com/example/parvatiarchana/LinerNestedActivity.java
@@ -12,6 +12,8 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.parvatiarchana.imageloder.ImageLoader;
+
 import java.io.InputStream;
 
 public class LinerNestedActivity extends AppCompatActivity {
@@ -23,25 +25,34 @@ public class LinerNestedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_liner_nested);
         System.out.println("LinerNestedActivity 2");
         /** Adding multiple Linerlayout in that image */
-        LinearLayout cat_linear=(LinearLayout) findViewById(R.id.linernestedlay);
+        LinearLayout cat_linear = (LinearLayout) findViewById(R.id.linernestedlay);
         System.out.println("LinerNestedActivity 3");
         ImageView imageView = new ImageView(LinerNestedActivity.this);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(100, 200);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(400 ,400);
         imageView.setLayoutParams(params);
         imageView.setImageResource(R.drawable.img2);
         System.out.println("LinerNestedActivity 4");
         TextView tv = new TextView(LinerNestedActivity.this);
 
         System.out.println("LinerNestedActivity 5");
-          LinearLayout ll1 = new LinearLayout(LinerNestedActivity.this);
-                       ll1.setOrientation(LinearLayout.VERTICAL);
-                    //   ll.setId();
+        LinearLayout ll1 = new LinearLayout(LinerNestedActivity.this);
+        ll1.setOrientation(LinearLayout.VERTICAL);
+        //   ll.setId();
         System.out.println("LinerNestedActivity 6");
-        String url1="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/16485962/2021/12/11/efac2f88-8856-4397-9dd2-3d680de385b61639214687618SochWomenBlackYokeDesignLayeredVelvetKurtiwithTrousers1.jpg";
-        setImage(imageView,url1);
-       ll1.addView(imageView);
-Button bt1 = new Button(LinerNestedActivity.this);
-bt1.setText("button1");
+        String url1 = "https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/16485962/2021/12/11/efac2f88-8856-4397-9dd2-3d680de385b61639214687618SochWomenBlackYokeDesignLayeredVelvetKurtiwithTrousers1.jpg";
+        setImage(imageView, url1);
+/* Not working
+       ImageLoader imgLoader = new ImageLoader(LinerNestedActivity.this);
+
+
+        int loader = R.drawable.img1;
+        imgLoader.DisplayImage(url1, loader,imageView);
+
+        */
+
+        ll1.addView(imageView);
+        Button bt1 = new Button(LinerNestedActivity.this);
+        bt1.setText("button1");
         ll1.addView(bt1);
         System.out.println("LinerNestedActivity 7");
         tv.setText("url1:");
@@ -49,13 +60,13 @@ bt1.setText("button1");
        /* tv[i][i].setLayoutParams(new LinearLayout.LayoutParams(
                 LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f));
 */
-    //..    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        //..    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         ll1.addView(tv);
         System.out.println("LinerNestedActivity 9");
         cat_linear.addView(ll1);
         System.out.println("LinerNestedActivity 10");
         ImageView imageView2 = new ImageView(LinerNestedActivity.this);
-      //  LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(100, 200);
+        //  LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(100, 200);
         imageView2.setLayoutParams(params);
         imageView2.setImageResource(R.drawable.img4);
         TextView tv2 = new TextView(LinerNestedActivity.this);
@@ -76,7 +87,7 @@ bt1.setText("button1");
         ll2.addView(tv2);
 
         cat_linear.addView(ll2);
-       // ll.addView(view, layoutParams);
+        // ll.addView(view, layoutParams);
 
         System.out.println("LinerNestedActivity End");
     }
@@ -101,18 +112,30 @@ bt1.setText("button1");
      *             }});
      */
 
-    /** Added at 27 jan 2022 */
-    private void setImage(ImageView bmImage,String url) {
-        String urldisplay = url;
+    /**
+     * Added at 27 jan 2022
+     */
+    private void setImage(ImageView bmImage, String url) {
+        new DownloadImageTask(bmImage).execute(url);
+
+
+       /* String urldisplay = url;
         Bitmap mIcon11 = null;
         try {
+            System.out.println(" Inside ste image..1");
             InputStream in = new java.net.URL(urldisplay).openStream();
+            System.out.println(" Inside ste image..2");
             mIcon11 = BitmapFactory.decodeStream(in);
+            System.out.println(" Inside ste image..3");
             bmImage.setImageBitmap(mIcon11);
+            System.out.println(" Inside ste image..4");
         } catch (Exception e) {
             // Log.e("Error", e.getMessage());
-
+e.printStackTrace();
         }
+
+*/
+
     }
 
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
@@ -122,7 +145,7 @@ bt1.setText("button1");
             this.bmImage = bmImage;
         }
 
-        protected Bitmap doInBackground(String... urls) {
+        protected  Bitmap doInBackground(String... urls) {
             String urldisplay = urls[0];
             Bitmap mIcon11 = null;
             try {
