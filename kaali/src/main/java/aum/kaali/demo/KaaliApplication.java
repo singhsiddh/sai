b64214923be66068d2b6b7c25b4fd717e9f672ac
singhsiddh <singhsiddh@yahoo.com>
2023-02-15 13:32:51 +0000

    Signed-off-by: singhsiddh <singhsiddh@yahoo.com>

--- kaali/src/main/java/aum/kaali/demo/KaaliApplication.java
+++ kaali/src/main/java/aum/kaali/demo/KaaliApplication.java
@@ -8,7 +8,14 @@ public class KaaliApplication {
 
 	public static void main(String[] args) {
 		System.out.println("jai ma Kaali");
+		try {
 		SpringApplication.run(KaaliApplication.class, args);
+		}catch(Throwable th) {
+			th.printStackTrace();
+			System.out.println("jai ma Kaali Error ");
+		}
+		
+		System.out.println("jai ma Kaali end ");
 	}
 
 }
