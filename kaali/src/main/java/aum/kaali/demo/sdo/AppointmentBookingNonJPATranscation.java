b8dac4ecc2d208bef9724c3efa7cf0d83fed8ad4
singhsiddh <singhsiddh@yahoo.com>
2023-03-04 20:15:54 +0000

    bug fix if availabily going below 0 do not allow to book
    Signed-off-by: singhsiddh <singhsiddh@yahoo.com>

--- kaali/src/main/java/aum/kaali/demo/sdo/AppointmentBookingNonJPATranscation.java
+++ kaali/src/main/java/aum/kaali/demo/sdo/AppointmentBookingNonJPATranscation.java
@@ -43,43 +43,36 @@ public class AppointmentBookingNonJPATranscation {
 	 */
 //hinmt :https://www.mongodb.com/docs/drivers/java/sync/current/fundamentals/aggregation/
 	public Integer getTotalAvailavleSlotByYearAndMonth(int yearIn, int monthIn) {
-	
+
 		MongoDatabase db = mongoClient.getDatabase("local");// TODO remove hardcoded value
-		 MongoCollection<org.bson.Document> collection = db.getCollection("AppointmentSlotData");
+		MongoCollection<org.bson.Document> collection = db.getCollection("AppointmentSlotData");
 
-		 collection.aggregate(
-				    Arrays.asList(
-				    		//Aggregates.addFields(fields),
-				    		Aggregates.match(Filters.eq("company","abc")),
-				    		Aggregates.match(Filters.eq("date","2023-02-24")),
-				    //		Aggregates.match(Filters.exp((new Document()).append("$in", Arrays.asList(new Document().append("$month", "$date"),monthIn)))),
-					        Aggregates.group("$_id",  Accumulators.sum("sum_slot", "$available")),
-					        Aggregates.project(
-				            Projections.fields(
-				                Projections.excludeId(),
-				                Projections.include("date","slotId")/*,
-				                Projections.computed(
-				                    "firstCategory",
-				                    new Document("$arrayElemAt", Arrays.asList("$categories", 0))
-				                )*/
-				            )
-				        )
-					      //  .and(DateOperators.Month.monthOf("date")).as("month")// end of project
-				        
-				        
-				    )
-				).forEach(doc -> System.out.println(doc.toJson()));
-		 
-		 
-		 
+		collection.aggregate(Arrays.asList(
+				// Aggregates.addFields(fields),
+				Aggregates.match(Filters.eq("company", "abc")), Aggregates.match(Filters.eq("date", "2023-02-24")),
+				// Aggregates.match(Filters.exp((new Document()).append("$in", Arrays.asList(new
+				// Document().append("$month", "$date"),monthIn)))),
+				Aggregates.group("$_id", Accumulators.sum("sum_slot", "$available")),
+				Aggregates.project(Projections.fields(Projections.excludeId(), Projections.include("date",
+						"slotId")/*
+									 * , Projections.computed( "firstCategory", new Document("$arrayElemAt",
+									 * Arrays.asList("$categories", 0)) )
+									 */
+				))
+		// .and(DateOperators.Month.monthOf("date")).as("month")// end of project
 
-	        // Gets the student with the highest average in the class.
-	   /*     AggregateIterable<Document> results = collection.aggregate(Arrays.asList(Aggregates.unwind("$scores"),
-	                Aggregates.group("$_id", Accumulators.avg("average", "$scores.score")),
-	                Aggregates.sort(Sorts.descending("average")), Aggregates.limit(1)));
-	                
-	                */
-		//UpdateResult result = db.getCollection("AppointmentSlotData").updateMany(searchQuery, setQuery);
+		)).forEach(doc -> System.out.println(doc.toJson()));
+
+		// Gets the student with the highest average in the class.
+		/*
+		 * AggregateIterable<Document> results =
+		 * collection.aggregate(Arrays.asList(Aggregates.unwind("$scores"),
+		 * Aggregates.group("$_id", Accumulators.avg("average", "$scores.score")),
+		 * Aggregates.sort(Sorts.descending("average")), Aggregates.limit(1)));
+		 * 
+		 */
+		// UpdateResult result =
+		// db.getCollection("AppointmentSlotData").updateMany(searchQuery, setQuery);
 
 		return 0;
 	}
@@ -105,7 +98,9 @@ public class AppointmentBookingNonJPATranscation {
 		// greater than slected number
 		System.out.println("searchQuery" + searchQuery);
 		int newAvailability = currentData.getAvailable() - transactionData.getTotalReservation();
-
+		if (newAvailability < 0) {
+			return false;
+		}
 		int reserved = currentData.getReserved() + transactionData.getTotalReservation();
 
 		BasicDBObject updateFields = new BasicDBObject();
