b2d6de180255f8e8ee8263285bbf4f3b71265cb1
singhsiddh <singhsiddh@yahoo.com>
2023-05-25 12:00:07 +0100

    Test case added
    Signed-off-by: singhsiddh <singhsiddh@yahoo.com>

--- Readme.txt
+++ Readme.txt
@@ -9,3 +9,23 @@ Code Branc stratigy :
 Need to discuss from whic branch QA ,UAT pipleline take code 
 one option crate JAR from Custom deploy in QA and after QA passed then merge in QA branch then create jar from QA and deploy in UAT and once UAT passed merge in UAT branch and one UAT pass then merge in UAT ..
 and final JAR will fo Nexus repository 
+
+
+collection
+
+List -->ArrayList ,LinkedList
+Set -->HashSet,LinkedHashSet,TreeSet ( sorted Set)
+Map-->Hashmap,TreeMap,ConcurenacehashMap( Bucket level Lock) 
+
+HashSet: If you don’t want to maintain insertion order but want to store unique objects. 
+LinkedHashSet: If you want to maintain the insertion order of elements then you can use LinkedHashSet. 
+TreeSet: If you want to sort the elements according to some Comparator then use TreeSet.
+
+266
+
+
+TreeMap is an example of a SortedMap, which means that the order of the keys can be sorted, and when iterating over the keys, you can expect that they will be in order.
+
+HashMap on the other hand, makes no such guarantee. Therefore, when iterating over the keys of a HashMap, you can't be sure what order they will be in.
+
+HashMap will be more efficient in general, so use it whenever you don't care about the order of the keys.
--- kaali/src/test/java/aum/kaali/demo/KaaliApplicationTests.java
+++ kaali/src/test/java/aum/kaali/demo/KaaliApplicationTests.java
@@ -1,13 +1,51 @@
 package aum.kaali.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import aum.kaali.demo.bo.AppointmentSlotData;
+import aum.kaali.demo.bo.SlotReservationData;
+import aum.kaali.demo.sdo.AppointmentRepository;
+import aum.kaali.demo.sdo.AppointmentSlotMetadataRepository;
+import aum.kaali.demo.so.AppointmentBookingService;
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class KaaliApplicationTests {
-
+@Autowired 
+AppointmentBookingService bService;
 	@Test
 	void contextLoads() {
 	}
+	@MockBean
+	private AppointmentRepository appointmentRepository;
+	 
+	@MockBean
+	private AppointmentSlotMetadataRepository appointmentSlotRepository;
+	@Test
+	public void addAppoirntment(SlotReservationData in) {
+		//when(appointmentRepository.save(in)).thenReturn()
+		appointmentRepository.save(in);
+		//assertEqual()
 
+	}
+
+	@Test
+	public void findAppointmentSlotDataByDateAndSlotId(Date date, Integer id) {
+		System.out.println( "Inside Test sai 25 May 2023");
+		AppointmentSlotData data = new AppointmentSlotData();
+		data.setAvailable(2);
+		
+		when(appointmentSlotRepository.findByDateAndSlotId(date, id)).thenReturn(data);
+		AppointmentSlotData data1=	 bService.findAppointmentSlotDataByDateAndSlotId(date, id);
+		 assertEquals(2,data1.getAvailable());
+	}
 }
